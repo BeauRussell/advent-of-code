559795f1d57a0012e03dc9c43646801ded70d65c
Beau Russell <yourknightmares@gmail.com>
2025-12-05 19:59:24 -0600

    2025 Day 2

--- 2025/02/index.ts
+++ 2025/02/index.ts
@@ -1,1 +1,49 @@
-console.log("Hello via Bun!");
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+let password = 0;
+const filePath = 'data.txt';
+
+async function run(): Promise<void> {
+	try {
+		const fileStream = fs.createReadStream(filePath);
+
+		const rl = readline.createInterface({
+			input: fileStream,
+			crlfDelay: Infinity
+		});
+
+		rl.on('line', (line: string) => {
+			processInput(line);
+		});
+
+		await new Promise<void>((resolve) => {
+			rl.on('close', () => {
+				console.log(password);
+				resolve();
+			});
+		});
+	} catch (error) {
+		console.error("Error reading file:", error);
+	}
+}
+
+function processInput(line: string): void {
+	const ranges = line.split(',');
+
+	for (const range of ranges) {
+		const arr = range.split('-');
+		const min = Number(arr[0]);
+		const max = Number(arr[1]);
+
+		for (let i = min; i <= max; i++) {
+			const string = i.toString();
+
+			if (/^(\d+)\1+$/.test(string)) {
+				password += i;
+			}
+		}
+	}
+}
+
+run();
